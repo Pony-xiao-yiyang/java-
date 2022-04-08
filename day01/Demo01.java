c0c9d41d55c4fc0e4794e9469692a6e008c019a0
Administrator <ponyxiaoyiyang.gmail.com>
2022-04-08 13:27:51 +0800

    java学习

---
+++ day01/Demo01.java
@@ -0,0 +1,28 @@
+package day01;
+
+/**
+ * @description:java程序设计概述
+ * @author:Linus Guderian
+ * @time: 2022/4/8 12:50
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        /**
+         * 一.java的特性
+         *      1.简单性
+         *      2.面向对象
+         *      3.分布式
+         *      4.健壮性
+         *      5.安全性
+         *      6.体系结构中立
+         *      7.可移植性
+         *      8.解释性
+         *      9.高性能
+         *      10.多线程
+         *      11.动态性
+         *
+         */
+        System.out.println("hello,world");
+        System.out.println("we will not use ‘hello,world！’");
+    }
+}
---
+++ day01/Demo02.java
@@ -0,0 +1,12 @@
+package day01;
+
+/**
+ * @description:
+ * @author:
+ * @time: 2022/4/8 13:09
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+
+    }
+}

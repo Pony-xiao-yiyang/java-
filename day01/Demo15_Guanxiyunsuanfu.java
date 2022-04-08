2dddc68537143e51aad4458372a33b07c0ae30b0
Administrator <ponyxiaoyiyang.gmail.com>
2022-04-08 18:33:03 +0800

    java学习

---
+++ day01/Demo15_Guanxiyunsuanfu.java
@@ -0,0 +1,56 @@
+package day01;
+
+/**
+ * @description:关系运算符
+ * @author:
+ * @time: 2022/4/8 18:20
+ */
+public class Demo15_Guanxiyunsuanfu {
+    public static void main(String[] args) {
+        /**
+         * 关系运算符
+         *          ==      a==b    判断a和b的值是否相等，成立为true，不成立为false；
+         *          !=      a!=b    判断a和b的值是否不相等，成立为true，不成立为false；
+         *          >       a>b     判断a是否大于b，成立为true，不成立为false；
+         *          >=      a>=b    判断a是否大于等于b，成立为true，不成立为false；
+         *          <       a<=b    判断a是否小于b，成立为true，不成立为false；
+         *          <=      a<=b    判断a是否小于等于b，成立为true，不成立为false；
+         */
+
+        //定义变量
+        int i = 10;
+        int j = 20;
+        int k = 10;
+
+        //==
+        System.out.println(i == j);
+        System.out.println(i == k);
+        System.out.println("=========================================");
+
+        //!=
+        System.out.println(i != j);
+        System.out.println(i != k);
+        System.out.println("=========================================");
+
+        //>
+        System.out.println(i > j);
+        System.out.println(i > k);
+        System.out.println("=========================================");
+
+        //>=
+        System.out.println(i >= j);
+        System.out.println(i >= k);
+        System.out.println("=========================================");
+
+        //<
+        System.out.println(i < j);
+        System.out.println(i < k);
+        System.out.println("=========================================");
+
+        //<=
+        System.out.println(i <= j);
+        System.out.println(i <= k);
+        System.out.println("=========================================");
+
+    }
+}
